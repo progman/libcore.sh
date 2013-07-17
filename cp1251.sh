777606a53e12ce70f9678bfdb5e326deaec7cf25
progman <gnuplanet@gmail.com>
2013-07-17 11:47:35 +0400

    add cp1251.sh

---
+++ cp1251.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+TMP="$(mktemp)";
+
+ls -1 > "${TMP}";
+
+
+while read -r i;
+do
+	j=$(echo "${i}" | iconv -fcp1251);
+#	echo "${i} -> ${j}";
+
+
+	if [ "${1}" != "-f" ];
+	then
+		echo "\"${j}\"";
+	else
+		if [ "${i}" != "${j}" ];
+		then
+			echo "\"${j}\"";
+			mv "${i}" "${j}";
+		fi
+	fi
+
+
+done < "${TMP}";
+
+rm "${TMP}" &> /dev/null;
