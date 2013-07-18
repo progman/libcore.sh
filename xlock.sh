ef626990072ab1b432d508f040004405726885f2
progman <gnuplanet@gmail.com>
2013-07-18 13:11:12 +0400

    update

---
+++ xlock.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export LANG='ru_RU.UTF-8';
+export LANGUAGE="${LANG}";
+export LC_CTYPE="${LANG}";
+export LC_NUMERIC="${LANG}";
+export LC_TIME="${LANG}";
+export LC_COLLATE="${LANG}";
+export LC_MONETARY="${LANG}";
+export LC_MESSAGES="${LANG}";
+export LC_PAPER="${LANG}";
+export LC_NAME="${LANG}";
+export LC_ADDRESS="${LANG}";
+export LC_TELEPHONE="${LANG}";
+export LC_MEASUREMENT="${LANG}";
+export LC_IDENTIFICATION="${LANG}";
+export LC_ALL="${LANG}";
+
+#xlock -mode blank;
+xset dpms force off;
+xlock -mode blank -dpmsstandby 0 -dpmssuspend 0 -dpmsoff 0 +resetsaver;
