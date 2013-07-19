2c09d1a6993441fd32634636009fded78549ad90
progman <gnuplanet@gmail.com>
2013-07-19 13:00:26 +0400

    fix style

--- xmms.sh
+++ xmms.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "$@" > /tmp/xmms
-audacious "$@" &> /dev/null < /dev/null &
-#audacious2 "$@" &> /tmp/audacious2 < /dev/null &
+echo "${@}" > /tmp/xmms;
+audacious "${@}" &> /dev/null < /dev/null &
