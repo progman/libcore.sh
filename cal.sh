d01e4d76312c4db2c9a6608aa9cd9f9320e6f7c0
progman <gnuplanet@gmail.com>
2013-07-22 18:02:01 +0400

    add cal.sh

---
+++ cal.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#date
+#echo
+#cal
+#sleep 10
+
+
+xterm -title "calendar" -geometry 28x11 -sl 15000 -bc -cr yellow -bg black -fg orange -hc orange4 -font '-cronyx-fixed-medium-r-normal-*-*-120-*-*-c-*-koi8-r' -e 'date; echo; cal; sleep 10' &
+#sleep 10
