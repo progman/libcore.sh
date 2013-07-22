b21d44bb67563dd2602f45957200c43cb22df18b
progman <gnuplanet@gmail.com>
2013-07-22 16:56:34 +0400

    add smartgit.sh

---
+++ boot/smartgit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export SMARTGIT_JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.26;
+export JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.26;
+/opt/smartgit/bin/smartgit.sh > /dev/null < /dev/null &
