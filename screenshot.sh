d1813a792bcfa547d0c2635b1f6f3eac11d17978
progman <gnuplanet@gmail.com>
2013-07-23 15:15:07 +0400

    add pngcrush

--- screenshot.sh
+++ screenshot.sh
@@ -9,3 +9,16 @@ fi
 FILENAME="/tmp/screenshot-$(date '+%Y%m%d_%H%M%S').png";
 
 import -window "${WINDOW}" "${FILENAME}";
+
+
+if [ "$(which pngcrush)" != "" ];
+then
+	TMP="$(mktemp)";
+	pngcrush -brute "${FILENAME}" "${TMP}";
+	if [ "${?}" == "0" ];
+	then
+		mv "${TMP}" "${FILENAME}";
+	else
+		rm -rf "${TMP}";
+	fi
+fi
