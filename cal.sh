6b5d5c667d739ad3ad904b9c86b41dcb4d588e55
progman <gnuplanet@gmail.com>
2013-07-25 14:18:13 +0400

    add exit code

--- cal.sh
+++ cal.sh
@@ -8,3 +8,5 @@
 
 xterm -title "calendar" -geometry 28x11 -sl 15000 -bc -cr yellow -bg black -fg orange -hc orange4 -font '-cronyx-fixed-medium-r-normal-*-*-120-*-*-c-*-koi8-r' -e 'date; echo; cal; sleep 10' &
 #sleep 10
+
+exit 0;
