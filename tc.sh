3c158af5fd844e7a3ff0f062f8f2642006083192
progman <gnuplanet@gmail.com>
2013-07-25 14:17:45 +0400

    replace stupid increment

--- tc.sh
+++ tc.sh
@@ -2,27 +2,20 @@
 
 # echo "alias tc='. tc.sh'" >> ~/.bashrc
 
+
 X=0;
+LAST=$(find /tmp/ -type d -iname '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' | sort -nr | head -n 1 | sed -e 's/.*\///g' | sed -e 's/^0*//g');
 
-while true;
-do
+if [ "${LAST}" != "" ];
+then
+	X="${LAST}";
 	Y=$(printf "/tmp/%08u" ${X});
-
+else
+	Y=$(printf "/tmp/%08u" ${X});
 	if [ ! -e "${Y}" ];
 	then
-		if [ "${X}" == "0" ];
-		then
-			mkdir "${Y}";
-			cd "${Y}";
-		else
-			(( X-- ));
-			Y=$(printf "/tmp/%08u" ${X});
-			cd "${Y}";
-		fi
-
-		break;
+		mkdir "${Y}";
 	fi
+fi
 
-	(( X++ ));
-
-done
+cd "${Y}";
--- tn.sh
+++ tn.sh
@@ -2,20 +2,22 @@
 
 # echo "alias tn='. tn.sh'" >> ~/.bashrc
 
+
 X=0;
+LAST=$(find /tmp/ -type d -iname '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' | sort -nr | head -n 1 | sed -e 's/.*\///g' | sed -e 's/^0*//g');
 
-while true;
-do
-	Y=$(printf "/tmp/%08u" ${X});
-
-	if [ ! -e "${Y}" ];
-	then
-		mkdir "${Y}";
-		cd "${Y}";
-
-		break;
-	fi
-
+if [ "${LAST}" != "" ];
+then
+	X="${LAST}";
 	(( X++ ));
+else
+	Y=$(printf "/tmp/%08u" ${X});
+	if [ -e "${Y}" ];
+	then
+		(( X++ ));
+	fi
+fi
 
-done
+Y=$(printf "/tmp/%08u" ${X});
+mkdir "${Y}";
+cd "${Y}";
