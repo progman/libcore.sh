8e2d336c6eac83bfbe96983704d67ed4d8d54793
progman <gnuplanet@gmail.com>
2013-08-09 22:17:22 +0400

    disable screensaver

--- boot/xlock.sh
+++ boot/xlock.sh
@@ -19,5 +19,9 @@ export LC_ALL="${LANG}";
 sleep 1;
 #xlock -mode blank;
 #xset s off;
+xset +dpms;
 xset dpms force on;
+#beep -l 0.1 -r 3
 xlock -mode blank -dpmsstandby 1 -dpmssuspend 1 -dpmsoff 1 +resetsaver;
+xset s off;
+xset -dpms;
