36d2686c7ae7d53bad1e4a186de837ee591ab58d
progman <gnuplanet@gmail.com>
2013-08-09 22:40:32 +0400

    fix style

--- image-small.sh
+++ image-small.sh
@@ -1,31 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 # (c) progman http://www.gnuplanet.ru
 # script using imagemagick
 
 if [ "${1}" == "" ];
 then
-    echo "image-small version 0.0.3"
-    echo "example: ${0} /tmp/image-dir/"
-    exit 0
+	echo "image-small version 0.0.3"
+	echo "example: ${0} /tmp/image-dir/"
+	exit 0
 fi
 
 cd "${1}"
 if [ "${?}" == "0" ];
 then
 
-    for file in *;
-    do
+	for file in *;
+	do
 
-	if [ "$(echo ${file} | grep -i 'png$\|jpg$\|jpeg$\|gif$\|bmp$\|pcx$' | wc -l)" != "0" ];
-	then
+		if [ "$(echo ${file} | grep -i 'png$\|jpg$\|jpeg$\|gif$\|bmp$\|pcx$' | wc -l)" != "0" ];
+		then
 
-	    echo "convert \"${file}\"";
-	    convert -geometry 107x80 "${file}" "${file}-small.jpg";
+			echo "convert \"${file}\"";
+			convert -geometry 107x80 "${file}" "${file}-small.jpg";
 
-	fi
+		fi
 
-    done
+	done
 fi
 
 exit 0;
