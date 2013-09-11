0a1d998cc896b5e01d80f37fcbb2378ac46b7618
progman <gnuplanet@gmail.com>
2013-09-11 11:50:39 +0400

    change dir name

--- boot/smartgit.sh
+++ boot/smartgit.sh
@@ -2,4 +2,4 @@
 
 export SMARTGIT_JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.26;
 export JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.26;
-/opt/smartgit/bin/smartgit.sh > /dev/null < /dev/null &
+/opt/smartgithg/bin/smartgithg.sh > /dev/null < /dev/null &
