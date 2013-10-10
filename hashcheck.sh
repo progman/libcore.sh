5dd5a7f290b207bc835d37aea2a4813369dd99bf
progman <gnuplanet@gmail.com>
2013-10-10 14:47:58 +0400

    add sha1, sha224, sha256, sha384, sha512

--- hashcheck.sh
+++ hashcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.1
+# 0.0.2
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -22,18 +22,58 @@ function check_prog()
 function main()
 {
 # check depends tools
-	check_prog "echo find grep md5sum";
+	check_prog "echo find grep md5sum sha1sum sha224sum sha256sum sha384sum sha512sum";
 	if [ "${?}" != "0" ];
 	then
 		return 1;
 	fi
 
 
-	FILE_LIST="$(find ./ -type f | grep 'md5$')";
+	FILE_LIST="$(find ./ -type f | grep '\.md5$')";
 	for i in ${FILE_LIST};
 	do
-		echo "check ${i}";
+		echo -ne "check ${i}\n\t";
 		md5sum -c "${i}";
+	done
+
+
+	FILE_LIST="$(find ./ -type f | grep '\.sha1$')";
+	for i in ${FILE_LIST};
+	do
+		echo -ne "check ${i}\n\t";
+		sha1sum -c "${i}";
+	done
+
+
+	FILE_LIST="$(find ./ -type f | grep '\.sha224$')";
+	for i in ${FILE_LIST};
+	do
+		echo -ne "check ${i}\n\t";
+		sha224sum -c "${i}";
+	done
+
+
+	FILE_LIST="$(find ./ -type f | grep '\.sha256$')";
+	for i in ${FILE_LIST};
+	do
+		echo -ne "check ${i}\n\t";
+		sha256sum -c "${i}";
+	done
+
+
+	FILE_LIST="$(find ./ -type f | grep '\.sha384$')";
+	for i in ${FILE_LIST};
+	do
+		echo -ne "check ${i}\n\t";
+		sha384sum -c "${i}";
+	done
+
+
+	FILE_LIST="$(find ./ -type f | grep '\.sha512$')";
+	for i in ${FILE_LIST};
+	do
+		echo -ne "check ${i}\n\t";
+		sha512sum -c "${i}";
 	done
 
 
