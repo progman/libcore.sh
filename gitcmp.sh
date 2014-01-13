9c4e41d1b1e53e1b80a4e6ef248051e154ffa343
progman <gnuplanet@gmail.com>
2014-01-13 16:13:28 +0400

    add cmp_branch_inner()

--- gitcmp.sh
+++ gitcmp.sh
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.4
+# 0.0.5
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -324,7 +324,7 @@ function cmp_repo()
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # compare all branch
-function cmp_branch()
+function cmp_branch_cross()
 {
 # save current dir
 	DIR_CUR="${PWD}";
@@ -415,10 +415,136 @@ function cmp_branch()
 	return 0;
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# compare two branch
+function cmp_branch_inner()
+{
+# save current dir
+	DIR_CUR="${PWD}";
+
+
+# create tmp file
+	local TMP1;
+	TMP1="$(mktemp)";
+	if [ "${?}" != "0" ];
+	then
+		echo "can't make tmp file";
+		return 1;
+	fi
+
+
+# create tmp file
+	local TMP2;
+	TMP2="$(mktemp)";
+	if [ "${?}" != "0" ];
+	then
+		echo "can't make tmp file";
+		rm -rf -- "${TMP1}";
+		return 1;
+	fi
+
+
+# create branch list for dir1
+	cd -- "${1}";
+	if [ "$(git log 2> /dev/null | head -n 1 | grep '^commit' | wc -l)" == "0" ];
+	then
+		echo "ERROR: \"${1}\" is not GIT dir";
+		cd -- "${DIR_CUR}";
+		rm -rf -- "${TMP1}";
+		return 1;
+	fi
+	GITDIR1_STATUS="$(get_status)";
+
+	git branch | sed -e 's/^*\ //g' |
+	{
+		while read -r BRANCH;
+		do
+
+			echo -n "${BRANCH} ";
+			git log "${BRANCH}" 2> /dev/null | head -n 1 | grep '^commit' | { read a b; echo ${b}; };
+
+		done > "${TMP1}";
+	}
+	cd -- "${DIR_CUR}";
+
+
+# check branches
+	local FLAG_FOUND1=0;
+	local FLAG_FOUND2=0;
+	while read -r BRANCH1 HASH1;
+	do
+		if [ "${BRANCH1}" == "${2}" ];
+		then
+			FLAG_FOUND1=1;
+		fi
+
+		if [ "${BRANCH1}" == "${3}" ];
+		then
+			FLAG_FOUND2=1;
+		fi
+	done < "${TMP1}";
+
+	if [ "${FLAG_FOUND1}" != "1" ];
+	then
+		echo "ERROR: branch \"${2}\" not found";
+		return 1;
+	fi
+
+	if [ "${FLAG_FOUND2}" != "1" ];
+	then
+		echo "ERROR: branch \"${3}\" not found";
+		return 1;
+	fi
+
+
+# show status
+	if [ "${GITDIR1_STATUS}" != "(?)" ] && [ "${GITDIR1_STATUS}" != "" ];
+	then
+		echo "WARNING: find UNCOMMINTED files!";
+	fi
+
+
+
+	git log "${2}" 2> /dev/null | grep '^commit' | { while read a b; do echo ${b}; done } > "${TMP1}";
+	git log "${3}" 2> /dev/null | grep '^commit' | { while read a b; do echo ${b}; done } > "${TMP2}";
+
+
+	while read -r HASH1;
+	do
+		while read -r HASH2;
+		do
+
+			if [ "${HASH1}" == "${HASH2}" ];
+			then
+				echo "${HASH1}";
+
+				rm -rf -- "${TMP1}";
+				rm -rf -- "${TMP2}";
+				return 0;
+			fi
+
+		done < "${TMP2}";
+	done < "${TMP1}";
+
+
+	echo "WARNING: common commit not found";
+
+
+	rm -rf -- "${TMP1}";
+	rm -rf -- "${TMP2}";
+
+
+	return 1;
+}
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # view help
 function help()
 {
-	echo "example: ${0} GITDIR1 [GITDIR2]";
+	echo "example: ${0} GITDIR1 GITDIR2";
+	echo "    comparison of branches with the same name";
+	echo "example: ${0} GITDIR";
+	echo "    comparison of all branches with all"
+	echo "example: ${0} GITDIR BRANCH1 BRANCH2";
+	echo "    search last common commit"
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # general function
@@ -432,7 +558,7 @@ function main()
 	fi
 
 
-	if [ "${#}" != 1 ] && [ "${#}" != 2 ];
+	if [ "${#}" != 1 ] && [ "${#}" != 2 ] && [ "${#}" != 3 ];
 	then
 		help "${@}";
 		return 1;
@@ -447,20 +573,33 @@ function main()
 			return 1;
 		fi
 
-		cmp_branch "${@}";
+		cmp_branch_cross "${@}";
 		return "${?}";
 	fi
 
 
+	if [ "${#}" == 2 ];
+	then
+		if [ ! -d "${1}" ] || [ ! -d "${2}" ];
+		then
+			help "${@}";
+			return 1;
+		fi
 
-	if [ ! -d "${1}" ] || [ ! -d "${2}" ];
+
+		cmp_repo "${@}";
+		return "${?}";
+	fi
+
+
+	if [ ! -d "${1}" ];
 	then
 		help "${@}";
 		return 1;
 	fi
 
 
-	cmp_repo "${@}";
+	cmp_branch_inner "${@}";
 	return "${?}";
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
