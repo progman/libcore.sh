7374b210fafbd0e7cdbdbf1b23f7ef84e8bef768
progman <gnuplanet@gmail.com>
2016-04-07 18:41:19 +0300

    update

--- boot/chrome-tor.sh
+++ boot/chrome-tor.sh
@@ -3,7 +3,20 @@
 #apt-get install tor
 
 TMP="${HOME}/.config/google-chrome-tor";
-rm -rf "${TMP}";
-mkdir -p "${TMP}";
+mkdir -m 0700 ~/.config &> /dev/null;
+if [ "${?}" != "0" ];
+then
+	echo "ERROR: can not make dir";
+	exit 1;
+fi
+rm -rf -- "${TMP}" &> /dev/null;
+mkdir -p -- "${TMP}" &> /dev/null;
+if [ "${?}" != "0" ];
+then
+	echo "ERROR: can not make dir";
+	exit 1;
+fi
 google-chrome --proxy-server="socks5://127.0.0.1:9050" --incognito --user-data-dir="${TMP}";
-rm -rf "${TMP}";
+rm -rf -- "${TMP}" &> /dev/null;
+
+exit 0;
