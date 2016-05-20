0d9efd3acb74b2d473d06e7330a9f09f0e7586a1
progman <gnuplanet@gmail.com>
2016-05-20 17:15:19 +0300

    mv.sh: add make tmp dirs

--- mv.sh
+++ mv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.1
+# 0.0.2
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -29,22 +29,68 @@ function main()
 	fi
 
 
-	if [ "${1}" == "-h" ] || [ "${1}" == "-help" ] || [ "${1}" == "--help" ] || [ ! -d "${1}" ];
+	if [ "${1}" == "-h" ] || [ "${1}" == "-help" ] || [ "${1}" == "--help" ];
 	then
-		echo "example: ls -1 | ${0} DIR";
+		echo "example: ls -1 | ${0} [ DIR | --tmp [LIMIT] ]";
 		return 1;
 	fi
 
 
-	local FILE;
-	while read -r FILE;
-	do
-		if [ -e "${FILE}" ];
+	if [ "${1}" == "--tmp" ] || [ "${1}" == "--tmp" ];
+	then
+		local LIMIT=50;
+		if [ "${2}" != "" ];
 		then
-			mv "${FILE}" "${1}" &> /dev/null;
-			echo "mv \"${FILE}\" ${1}";
+			LIMIT="${2}";
 		fi
-	done
+
+		if [ "${LIMIT}" != "0" ];
+		then
+			local OFFSET=0;
+			local DIR;
+			local FILE;
+			while read -r FILE;
+			do
+				if [ "${OFFSET}" == "${LIMIT}" ];
+				then
+					OFFSET=0;
+				fi
+
+				if [ "${OFFSET}" == "0" ];
+				then
+					DIR=$(mktemp -d --tmpdir=./ 2> /dev/null);
+					if [ "${?}" != "0" ];
+					then
+						echo "FATAL: can't make tmp dir";
+						return 1;
+					fi
+				fi
+
+				if [ -e "${FILE}" ];
+				then
+					mv "${FILE}" "${DIR}" &> /dev/null;
+					echo "mv \"${FILE}\" ${DIR}";
+					(( OFFSET++ ));
+				fi
+			done
+		fi
+	else
+		if [ ! -d "${1}" ];
+		then
+			echo "FATAL: dir \"${1}\" not found...";
+			return 1;
+		fi
+
+		local FILE;
+		while read -r FILE;
+		do
+			if [ -e "${FILE}" ];
+			then
+				mv "${FILE}" "${1}" &> /dev/null;
+				echo "mv \"${FILE}\" ${1}";
+			fi
+		done
+	fi
 
 
 	return 0;
