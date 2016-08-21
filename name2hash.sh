d0ad5ecfde154750eaf4445c0f066c7130be2e51
progman <gnuplanet@gmail.com>
2016-08-21 13:26:50 +0300

    add read from stdin

--- name2hash.sh
+++ name2hash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.2
+# 0.0.3
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -36,12 +36,12 @@ function do_it()
 		NEW="${HASH}.${EXT}";
 	fi
 
-	echo "${NEW}";
+#	echo "${NEW}";
 
-	if [ -e "${NEW}" ];
-	then
-		return 2;
-	fi
+#	if [ -e "${NEW}" ];
+#	then
+#		return 2;
+#	fi
 
 	mv "${1}" "${NEW}";
 	if [ "${?}" != "0" ];
@@ -55,17 +55,18 @@ function do_it()
 # general function
 function main()
 {
-	local FILE_COUNT="${#}";
+#	local FILE_COUNT="${#}";
 
-	if [ "${1}" == "" ] || [ ! -e "${1}" ];
+
+	if [ "${1}" == "-h" ] || [ "${1}" == "-help" ] || [ "${1}" == "--help" ];
 	then
-		echo "example: ${0} FILE";
+		echo "example: ls -1 *.* | ${0}";
 		return 1;
 	fi
 
 
 # check depends tools
-	check_prog "echo awk sed sha3sum mv"; # sha3sum from libdigest-sha3-perl
+	check_prog "echo awk sed sha3sum mv mktemp wc"; # sha3sum from libdigest-sha3-perl
 	if [ "${?}" != "0" ];
 	then
 		return 1;
@@ -73,18 +74,50 @@ function main()
 
 
 # do list
-	while true;
+#	while true;
+#	do
+#		do_it "${1}";
+#
+#		(( FILE_COUNT-- ));
+#		shift 1;
+#
+#		if [ "${FILE_COUNT}" == "0" ];
+#		then
+#			break;
+#		fi
+#	done
+
+
+	local TMP;
+	TMP=$(mktemp 2> /dev/null);
+
+
+	while read -r FILE;
 	do
-		do_it "${1}";
-
-		(( FILE_COUNT-- ));
-		shift 1;
-
-		if [ "${FILE_COUNT}" == "0" ];
-		then
-			break;
-		fi
+		echo "${FILE}" >> "${TMP}";
 	done
+
+
+	local COUNT_CUR=1;
+	local COUNT_ALL;
+	COUNT_ALL=$(wc -l "${TMP}" | { read a b; echo ${a}; });
+
+
+# convert
+	while read -r FILE;
+	do
+		if [ -e "${FILE}" ];
+		then
+			echo "[${COUNT_CUR}/${COUNT_ALL}] ${FILE}";
+			do_it "${FILE}";
+		fi
+
+		(( COUNT_CUR++ ));
+
+	done < "${TMP}";
+
+
+	rm -- -rf "${TMP}" &> /dev/null;
 
 
 	return 0;
