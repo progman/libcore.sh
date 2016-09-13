6e6c73e5d41acccb522760f0f76dcf72c473ecbe
progman <gnuplanet@gmail.com>
2016-09-13 22:24:44 +0300

    rewrite make temp dir code

--- boot/chrome-tor.sh
+++ boot/chrome-tor.sh
@@ -2,21 +2,15 @@
 
 #apt-get install tor
 
-TMP="${HOME}/.config/google-chrome-tor";
-mkdir -m 0700 ~/.config &> /dev/null;
+LOCAL_TMP=$(mktemp -d);
 if [ "${?}" != "0" ];
 then
 	echo "ERROR: can not make dir";
 	exit 1;
 fi
-rm -rf -- "${TMP}" &> /dev/null;
-mkdir -p -- "${TMP}" &> /dev/null;
-if [ "${?}" != "0" ];
-then
-	echo "ERROR: can not make dir";
-	exit 1;
-fi
-google-chrome --proxy-server="socks5://127.0.0.1:9050" --incognito --user-data-dir="${TMP}";
-rm -rf -- "${TMP}" &> /dev/null;
+
+google-chrome --proxy-server="socks5://127.0.0.1:9050" --incognito --user-data-dir="${LOCAL_TMP}";
+
+rm -rf -- "${LOCAL_TMP}" &> /dev/null;
 
 exit 0;
