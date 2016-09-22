64484f9bf61df0019b5790ad7a301141f5e5b2af
progman <gnuplanet@gmail.com>
2016-09-22 19:25:48 +0300

    rewrite to_mp3.sh

--- to_mp3.sh
+++ to_mp3.sh
@@ -1,7 +1,252 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.2
+# 0.0.5
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# convert wav to mp3
+function convert_wav2mp3()
+{
+	local SOURCE="${1}";
+	local TARGET="${2}";
+
+
+# convert
+	lame --quiet -h -q 0 -v -V 0 -B 320 "${SOURCE}" "${TARGET}.tmp" < /dev/null;
+	if [ "${?}" != "0" ];
+	then
+		rm -rf -- "${TARGET}.tmp" &> /dev/null < /dev/null;
+		echo "ERROR[lame()]: unknown error";
+		return 1;
+	fi
+
+
+# rename
+	mv -- "${TARGET}.tmp" "${TARGET}" &> /dev/null < /dev/null;
+	if [ "${?}" != "0" ];
+	then
+		rm -rf -- "${TARGET}.tmp" &> /dev/null < /dev/null;
+		echo "ERROR[rename()]: unknown error";
+		return 1;
+	fi
+
+
+	return 0;
+}
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# convert something to wav
+function convert_something2wav()
+{
+	local SOURCE="${1}";
+	local TARGET="${2}";
+
+
+# convert
+	mplayer -vo null -ao pcm:file="${TARGET}.tmp" "${SOURCE}" &> /dev/null < /dev/null;
+	if [ "${?}" != "0" ];
+	then
+		rm -rf -- "${TARGET}.tmp" &> /dev/null < /dev/null;
+		echo "ERROR: unknown error";
+		return 1;
+	fi
+
+
+# rename
+	mv -- "${TARGET}.tmp" "${TARGET}" &> /dev/null < /dev/null;
+	if [ "${?}" != "0" ];
+	then
+		rm -rf -- "${TARGET}.tmp" &> /dev/null < /dev/null;
+		echo "ERROR[rename()]: unknown error";
+		return 1;
+	fi
+
+
+	return 0;
+}
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# convert something to mp3
+function convert()
+{
+	local SOURCE="${1}";
+	local TARGET;
+	local MIME="$(file -b -L --mime-type -- "${SOURCE}")";
+
+	local ALBUM_TITLE;
+	local CUR_TRACK;
+	local TRACK_COUNT;
+	local TRACK_TITLE;
+	local TRACK_PERFORMER;
+
+	local COMMENT;
+	local DATE;
+	local GENRE;
+
+	local LOCAL_TMPDIR="/tmp";
+	if [ "${TMPDIR}" != "" ] && [ -d "${TMPDIR}" ];
+	then
+		LOCAL_TMPDIR="${TMPDIR}";
+	fi
+
+
+	local TMP;
+	TMP="$(mktemp --tmpdir="${LOCAL_TMPDIR}" 2> /dev/null)";
+	if [ "${?}" != "0" ];
+	then
+		echo "can't make tmp file";
+		return 1;
+	fi
+
+
+	if [ "${MIME}" == "audio/x-wav" ];
+	then
+		TARGET=$(echo "${SOURCE}" | sed -e 's/\.wav$/\.mp3/gi');
+
+		convert_wav2mp3 "${SOURCE}" "${TARGET}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+		rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+
+		return 0;
+	fi
+
+
+	if [ "${MIME}" == "audio/x-flac" ];
+	then
+		TARGET=$(echo "${SOURCE}" | sed -e 's/\.flac$/\.mp3/gi');
+
+		ALBUM_TITLE=$(metaflac --show-tag ALBUM "${SOURCE}" | sed -e 's/.*=//g');
+		CUR_TRACK=$(metaflac --show-tag TRACKNUMBER "${SOURCE}" | sed -e 's/.*=//g');
+		TRACK_COUNT=$(metaflac --show-tag TOTALTRACKS "${SOURCE}" | sed -e 's/.*=//g');
+		TRACK_TITLE=$(metaflac --show-tag TITLE "${SOURCE}" | sed -e 's/.*=//g');
+		TRACK_PERFORMER=$(metaflac --show-tag ARTIST "${SOURCE}" | sed -e 's/.*=//g');
+
+		COMMENT=$(metaflac --show-tag COMMENT "${SOURCE}" | sed -e 's/.*=//g');
+		DATE=$(metaflac --show-tag DATE "${SOURCE}" | sed -e 's/.*=//g');
+		GENRE=$(metaflac --show-tag GENRE "${SOURCE}" | sed -e 's/.*=//g');
+
+
+		convert_something2wav "${SOURCE}" "${TMP}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+
+
+		convert_wav2mp3 "${TMP}" "${TARGET}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+
+
+		id3v2 -2 --album "${ALBUM_TITLE}" "${TARGET}"; # Set the album title information
+		id3v2 -2 --track "${CUR_TRACK}/${TRACK_COUNT}" "${TARGET}"; # Set the track number/(optional) total tracks
+		id3v2 -2 --song "${TRACK_TITLE}" "${TARGET}"; # Set the song title information
+		id3v2 -2 --artist "${TRACK_PERFORMER}" "${TARGET}"; # Set the artist information
+
+		id3v2 -2 --comment "${COMMENT}" "${TARGET}"; # Set comment
+
+		rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+
+
+		return 0;
+	fi
+
+
+	if [ "${MIME}" == "audio/x-ape" ];
+	then
+		TARGET=$(echo "${SOURCE}" | sed -e 's/\.ape$/\.mp3/gi');
+
+		ALBUM_TITLE=$(mplayer -vo null -ao null -identify -frames 0 "${SOURCE}" 2>&1 | grep '^ Album: ' | sed -e 's/^ Album: //g');
+		CUR_TRACK=$(mplayer -vo null -ao null -identify -frames 0 "${SOURCE}" 2>&1 | grep '^ Track: ' | sed -e 's/^ Track: //g');
+		TRACK_COUNT=$(mplayer -vo null -ao null -identify -frames 0 "${SOURCE}" 2>&1 | grep '^ Totaltracks: ' | sed -e 's/^ Totaltracks: //g');
+		TRACK_TITLE=$(mplayer -vo null -ao null -identify -frames 0 "${SOURCE}" 2>&1 | grep '^ Title: ' | sed -e 's/^ Title: //g');
+		TRACK_PERFORMER=$(mplayer -vo null -ao null -identify -frames 0 "${SOURCE}" 2>&1 | grep '^ Artist: ' | sed -e 's/^ Artist: //g');
+
+
+		convert_something2wav "${SOURCE}" "${TMP}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+
+
+		convert_wav2mp3 "${TMP}" "${TARGET}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+
+
+		id3v2 -2 --album "${ALBUM_TITLE}" "${TARGET}"; # Set the album title information
+		id3v2 -2 --track "${CUR_TRACK}/${TRACK_COUNT}" "${TARGET}"; # Set the track number/(optional) total tracks
+		id3v2 -2 --song "${TRACK_TITLE}" "${TARGET}"; # Set the song title information
+		id3v2 -2 --artist "${TRACK_PERFORMER}" "${TARGET}"; # Set the artist information
+#		id3v2 -2 --comment "${COMMENT}" "${TARGET}"; # Set comment
+
+		rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+
+
+		return 0;
+	fi
+
+
+
+	if [ "${MIME}" == "audio/ogg" ];
+	then
+		TARGET=$(echo "${SOURCE}" | sed -e 's/\.ogg$/\.mp3/gi');
+
+		ALBUM_TITLE=$(ogginfo "${SOURCE}" 2>&1 | grep -P '^\tALBUM=' | sed -e 's/.*ALBUM=//g');
+		CUR_TRACK=$(ogginfo "${SOURCE}" 2>&1 | grep -P '^\tTRACKNUMBER=' | sed -e 's/.*TRACKNUMBER=//g' | sed -e 's/\/.*//g');
+		TRACK_COUNT=$(ogginfo "${SOURCE}" 2>&1 | grep -P '^\tTRACKNUMBER=' | sed -e 's/.*TRACKNUMBER=//g' | sed -e 's/.*\///g');
+		TRACK_TITLE=$(ogginfo "${SOURCE}" 2>&1 | grep -P '^\tTITLE=' | sed -e 's/.*TITLE=//g');
+		TRACK_PERFORMER=$(ogginfo "${SOURCE}" 2>&1 | grep -P '^\tARTIST=' | sed -e 's/.*ARTIST=//g');
+
+
+#echo "ALBUM_TITLE: [${ALBUM_TITLE}]";
+#echo "CUR_TRACK: [${CUR_TRACK}]";
+#echo "TRACK_COUNT: [${TRACK_COUNT}]";
+#echo "TRACK_TITLE: [${TRACK_TITLE}]";
+#echo "TRACK_PERFORMER: [${TRACK_PERFORMER}]";
+
+
+		convert_something2wav "${SOURCE}" "${TMP}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+
+
+		convert_wav2mp3 "${TMP}" "${TARGET}";
+		if [ "${?}" != "0" ];
+		then
+			rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+			return 1;
+		fi
+
+
+		id3v2 -2 --album "${ALBUM_TITLE}" "${TARGET}"; # Set the album title information
+		id3v2 -2 --track "${CUR_TRACK}/${TRACK_COUNT}" "${TARGET}"; # Set the track number/(optional) total tracks
+		id3v2 -2 --song "${TRACK_TITLE}" "${TARGET}"; # Set the song title information
+		id3v2 -2 --artist "${TRACK_PERFORMER}" "${TARGET}"; # Set the artist information
+#		id3v2 -2 --comment "${COMMENT}" "${TARGET}"; # Set comment
+
+		rm -rf -- "${TMP}" &> /dev/null < /dev/null;
+
+
+		return 0;
+	fi
+
+
+	return 0;
+}
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
 function check_prog()
@@ -38,6 +283,11 @@ function main()
 
 	local TMP;
 	TMP=$(mktemp 2> /dev/null);
+	if [ "${?}" != "0" ];
+	then
+		echo "can't make tmp file";
+		return 1;
+	fi
 
 
 	while read -r FILE;
@@ -57,20 +307,14 @@ function main()
 		if [ -e "${FILE}" ];
 		then
 			echo "[${COUNT_CUR}/${COUNT_ALL}] ${FILE}";
-			lame --quiet -h -q 0 -v -V 0 -B 320 "${FILE}" "${FILE}.mp3.tmp" < /dev/null;
+
+			convert "${FILE}";
 			if [ "${?}" != "0" ];
 			then
-				echo "ERROR[lame()]: unknown error";
 				return 1;
 			fi
 
-# rename
-			mv -- "${FILE}.mp3.tmp" "${FILE}.mp3" &> /dev/null < /dev/null;
-			if [ "${?}" != "0" ];
-			then
-				echo "ERROR[rename()]: unknown error";
-				return 1;
-			fi
+#			rm -- -rf "${FILE}" &> /dev/null;
 		fi
 
 		(( COUNT_CUR++ ));
@@ -81,7 +325,7 @@ function main()
 	rm -- -rf "${TMP}" &> /dev/null;
 
 
-	return "${?}";
+	return 0;
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 main "${@}";
