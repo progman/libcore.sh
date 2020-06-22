cf6112d9f2b558b9b3645a08409bcbe5e565de55
progman <gnuplanet@gmail.com>
2020-06-22 12:36:55 +0300

    fix log_pack.sh

--- log_pack.sh
+++ log_pack.sh
@@ -4,6 +4,16 @@ for i in $(find . -type f | grep '/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0
 do
 
 	echo "${i}";
-	xz -9e -- "${i}";
+	xz -9ec -- "${i}" > "${i}.xz.tmp";
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR: pack error";
+		exit 1;
+	fi
+	mv "${i}.xz.tmp"; "${i}.xz";
+	rm -rf -- "${i}";
 
 done
+
+
+exit 0;
