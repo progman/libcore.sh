5d9b91c31c008a4cae7b5ebf666e9f96b9f2956c
progman <gnuplanet@gmail.com>
2021-11-23 21:45:34 +0300

    update

--- alarm_off.sh
+++ alarm_off.sh
@@ -4,6 +4,8 @@ if [ ! -f /tmp/FLAG_DISABLE_ALARM ];
 then
 	touch /tmp/FLAG_DISABLE_ALARM;
 	echo "alarm off";
+	echo "$(date '+%Y-%m-%d %H:%M:%S.%N'): alarm off" >> /tmp/alarm.log;
 else
 	echo "alarm already off";
+	echo "$(date '+%Y-%m-%d %H:%M:%S.%N'): alarm already off" >> /tmp/alarm.log;
 fi
--- alarm_on.sh
+++ alarm_on.sh
@@ -3,7 +3,9 @@
 if [ ! -f /tmp/FLAG_DISABLE_ALARM ];
 then
 	echo "alarm already on";
+	echo "$(date '+%Y-%m-%d %H:%M:%S.%N'): alarm already on" >> /tmp/alarm.log;
 else
 	rm -rf /tmp/FLAG_DISABLE_ALARM &> /dev/null;
 	echo "alarm on";
+	echo "$(date '+%Y-%m-%d %H:%M:%S.%N'): alarm on" >> /tmp/alarm.log;
 fi
