51d67a98db50e8747503d907887baa4a42b1d2ef
progman <gnuplanet@gmail.com>
2022-07-25 09:43:21 +0300

    update

--- h265.sh
+++ h265.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/58742765/convert-videos-from-264-to-265-hevc-with-ffmpeg
 
+# https://stackoverflow.com/questions/58742765/convert-videos-from-264-to-265-hevc-with-ffmpeg
 SOURCE="${1}";
 TARGET="${2}";
 
@@ -13,12 +13,32 @@ fi
 
 
 FLAG_NVIDIA=$(lspci | grep -i vga | grep -i NVIDIA | wc -l);
+
+
 if [ "${FLAG_NVIDIA}" == "0" ];
 then
+	echo "ffmpeg -i ${SOURCE} -c:v libx265 -vtag hvc1 -c:a copy ${TARGET}";
 	ffmpeg -i "${SOURCE}"                 -c:v libx265 -vtag hvc1 -c:a copy "${TARGET}";
 else
+	echo "ffmpeg -i ${SOURCE} -c:v hevc_nvenc -c:v libx265 -vtag hvc1 -c:a copy ${TARGET}";
 	ffmpeg -i "${SOURCE}" -c:v hevc_nvenc -c:v libx265 -vtag hvc1 -c:a copy "${TARGET}";
 fi
+
+
+if [ "${?}" != "0" ];
+then
+	rm -rf "${TARGET}" &> /dev/null < /dev/null;
+	echo "ERROR";
+	exit 1;
+fi
+
+
+#mv "${TARGET}.tmp" "${TARGET}" &> /dev/null < /dev/null;
+#if [ "${?}" != "0" ];
+#then
+#	echo "ERROR";
+#	exit 1;
+#fi
 
 
 exit 0;
