0644d5b2a6c5abe66cf17d8ce5057cf4ac633524
progman <gnuplanet@gmail.com>
2024-04-18 09:53:20 +0300

    update

--- example/cron_git_build.sh
+++ example/cron_git_build.sh
@@ -7,7 +7,7 @@ function build()
 
 	export DOCKER_DIR="${1}";
 	export DOCKER_NOTIFY_MSG="${2}";
-	/usr/local/bin/git_builder.sh;
+	/usr/local/bin/git_tool_builder.sh;
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 export TELEGRAM_BOT_TOKEN="TELEGRAM_BOT_TOKEN";
@@ -15,7 +15,7 @@ export TELEGRAM_CHAT_ID="TELEGRAM_CHAT_ID";
 export DOCKER_NOTIFY="/usr/local/bin/notify_telegram.sh";
 
 
-build '/tmp/xxx' "xxx builded";
+build '/tmp/xxx' "xxx builded"; # WARNING! Use special git dir for build - if you use one dir then push will be not detected
 
 
 sleep 10;
