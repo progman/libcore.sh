acb174879c751ebe2331878435dbd8e047a4b634
progman <gnuplanet@gmail.com>
2024-10-19 13:06:30 +0300

    rewrite lxc_export()

--- lxc_tool.sh
+++ lxc_tool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.1
+# 0.0.2
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -23,6 +23,7 @@ function lxc_ls()
 {
 	local STATUS;
 
+
 	lxc ls -c n -f compact | sed -e 's/\ //g' | grep -v '^NAME$'
 	STATUS="${?}";
 	if [ "${STATUS}" != "0" ];
@@ -30,31 +31,52 @@ function lxc_ls()
 		return "${STATUS}";
 	fi
 
+
 	return 0;
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 function lxc_export()
 {
 	local STATUS;
+	local FILE;
 
 
 	while read -r NAME;
 	do
-		echo "lxc export ${NAME} ./${NAME}.tar.gz --instance-only";
-		lxc export ${NAME} ./${NAME}.tar.gz --instance-only;
+		FILE="./${NAME}.tar";
+
+
+		echo "lxc export ${NAME} ${FILE} --instance-only --compression=none;";
+		lxc export ${NAME} ${FILE} --instance-only --compression=none;
 		STATUS="${?}";
 		if [ "${STATUS}" != "0" ];
 		then
 			echo "ERROR: export was broken";
 			return "${STATUS}";
 		fi
+
+
+		if [ "$(command -v zstd)" != "" ];
+		then
+			echo "zstd -C --ultra -22 --threads=0 -f -o ${FILE}.zst 2> /dev/null < ${FILE};";
+			zstd -C --ultra -22 --threads=0 -f -o "${FILE}.zst" 2> /dev/null < "${FILE}";
+			STATUS="${?}";
+			if [ "${STATUS}" != "0" ];
+			then
+				echo "ERROR: zstd pack was broken";
+				return "${STATUS}";
+			fi
+
+			echo "rm -rf ${FILE} &> /dev/null < /dev/null;";
+			rm -rf ${FILE} &> /dev/null < /dev/null;
+			STATUS="${?}";
+			if [ "${STATUS}" != "0" ];
+			then
+				echo "ERROR: rm was broken";
+				return "${STATUS}";
+			fi
+		fi
 	done
-
-
-#lxc export super-test /backups/super-test.tar.gz --instance-only;                                                                               # export в архив
-#lxc export super-test /backups/super-test.tar    --instance-only --compression=none; ls -1 super-test.tar | repack.sh --zstd;                   # export в архив
-
-#lxc import /backups/super-test.tar.zst;
 
 
 	return 0;
@@ -65,13 +87,9 @@ function lxc_import()
 	local STATUS;
 
 
-#	while read -r NAME;
 	while read -r FILE;
 	do
-#		echo "lxc import ./${NAME}.tar*";
-#		lxc import ./${NAME}.tar*;
-
-		echo "lxc import ${FILE}";
+		echo "lxc import ${FILE};";
 		lxc import "${FILE}";
 		STATUS="${?}";
 		if [ "${STATUS}" != "0" ];
@@ -80,12 +98,6 @@ function lxc_import()
 			return "${STATUS}";
 		fi
 	done
-
-
-#lxc export super-test /backups/super-test.tar.gz --instance-only;                                                                               # export в архив
-#lxc export super-test /backups/super-test.tar    --instance-only --compression=none; ls -1 super-test.tar | repack.sh --zstd;                   # export в архив
-
-#lxc import /backups/super-test.tar.zst;
 
 
 	return 0;
@@ -98,7 +110,7 @@ function lxc_start()
 
 	while read -r NAME;
 	do
-		echo "lxc start ${NAME}";
+		echo "lxc start ${NAME};";
 		lxc start ${NAME};
 		STATUS="${?}";
 		if [ "${STATUS}" != "0" ];
@@ -119,7 +131,7 @@ function lxc_stop()
 
 	while read -r NAME;
 	do
-		echo "lxc stop ${NAME}";
+		echo "lxc stop ${NAME};";
 		lxc stop ${NAME};
 		STATUS="${?}";
 		if [ "${STATUS}" != "0" ];
