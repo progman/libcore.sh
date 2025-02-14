fe59c47938357dd285d2a611516fdc5f55fd49e5
progman <gnuplanet@gmail.com>
2025-02-14 11:05:22 +0300

    update systemd.sh

--- systemd.sh
+++ systemd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.1
+# 0.0.2
 # Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -21,14 +21,184 @@ function check_prog()
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 function systemd_gc()
 {
-	journalctl --rotate
-	journalctl --vacuum-time=1s
+	journalctl --rotate;
+	journalctl --vacuum-time=1s;
+	return 0;
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function systemd_log()
+{
+	if [ ! -e ./.env ];
+	then
+		echo "ERROR: you must make .env file";
+		return 0;
+	fi
+
+	export $(cat .env);
+
+	if [ "${SYSTEMD_SERVICE}" == "" ];
+	then
+		echo "ERROR: you must set SYSTEMD_SERVICE";
+		return 1;
+	fi
+
+	journalctl -u ${SYSTEMD_SERVICE};
+
+	return 0;
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function systemd_status()
+{
+	if [ ! -e ./.env ];
+	then
+		echo "ERROR: you must make .env file";
+		return 0;
+	fi
+
+	export $(cat .env);
+
+	if [ "${SYSTEMD_SERVICE}" == "" ];
+	then
+		echo "ERROR: you must set SYSTEMD_SERVICE";
+		return 1;
+	fi
+
+	STATE="$(systemctl show ${SYSTEMD_SERVICE} | grep -- 'ActiveState=' | sed -e 's/.*=//g')";
+	echo "state: ${STATE}";
+
+	SUB_STATE="$(systemctl show ${SYSTEMD_SERVICE} | grep -- 'SubState=' | sed -e 's/.*=//g')";
+	echo "sub_state: ${SUB_STATE}";
+
+	systemctl status ${SYSTEMD_SERVICE};
+
+	return 0;
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function systemd_start()
+{
+	if [ ! -e ./.env ];
+	then
+		echo "ERROR: you must make .env file";
+		return 0;
+	fi
+
+	export $(cat .env);
+
+	if [ "${SYSTEMD_SERVICE}" == "" ];
+	then
+		echo "ERROR: you must set SYSTEMD_SERVICE";
+		return 1;
+	fi
+
+	systemctl restart rsyslog;
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR[start()]: 2";
+		return 1;
+	fi
+
+	systemctl daemon-reload &> /dev/null < /dev/null;
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR[start()]: 3";
+		return 1;
+	fi
+
+	systemctl start "${SYSTEMD_SERVICE}";
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR[start()]: 4";
+		return 1;
+	fi
+
+	STATE="$(systemctl show ${SYSTEMD_SERVICE} | grep -- 'ActiveState=' | sed -e 's/.*=//g')";
+	echo "state: ${STATE}";
+
+	if [ "${STATE}" != "active" ]; # ActiveState=active
+	then
+		echo "ERROR[start()]: 7";
+		return 1;
+	fi
+
+	SUB_STATE="$(systemctl show ${SYSTEMD_SERVICE} | grep -- 'SubState=' | sed -e 's/.*=//g')";
+	echo "sub_state: ${SUB_STATE}";
+
+	if [ "${SUB_STATE}" != "running" ]; # SubState=running
+	then
+		echo "ERROR[start()]: 8";
+		return 1;
+	fi
+
+	return 0;
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function systemd_stop()
+{
+	if [ ! -e ./.env ];
+	then
+		echo "ERROR: you must make .env file";
+		return 0;
+	fi
+
+	export $(cat .env);
+
+	if [ "${SYSTEMD_SERVICE}" == "" ];
+	then
+		echo "ERROR: you must set SYSTEMD_SERVICE";
+		return 1;
+	fi
+
+	STATE="$(systemctl show ${SYSTEMD_SERVICE} | grep -- 'ActiveState=' | sed -e 's/.*=//g')";
+	if [ "${STATE}" == "inactive" ]; # ActiveState=active
+	then
+		echo "ERROR[stop()]: 1";
+		return 0;
+	fi
+
+	systemctl stop "${SYSTEMD_SERVICE}";
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR[stop()]: 3";
+		return 1;
+	fi
+
+	if [ -e /var/log/www/${SYSTEMD_SERVICE}.log ];
+	then
+		UNIXTIME=$(date +'%s');
+		mv /var/log/www/${SYSTEMD_SERVICE}.log /var/log/www/${SYSTEMD_SERVICE}.log."${UNIXTIME}" &> /dev/null;
+		if [ "${?}" != "0" ];
+		then
+			echo "ERROR[stop()]: 4";
+			return 1;
+		fi
+	fi
+
+	return 0;
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function systemd_restart()
+{
+	systemd_stop;
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR[restart()]: 1";
+		return 1;
+	fi
+
+	systemd_start;
+	if [ "${?}" != "0" ];
+	then
+		echo "ERROR[restart()]: 2";
+		return 1;
+	fi
+
+	return 0;
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # show help
 function help()
 {
-	echo "example: ${1} [ gc ]";
+	echo "example: ${1} [ gc | log | status | start | stop | restart ]";
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # general function
@@ -41,6 +211,46 @@ function main()
 	if [ "${OPERATION}" == "gc" ]
 	then
 		systemd_gc;
+		STATUS="${?}";
+		return "${STATUS}";
+	fi
+
+
+	if [ "${OPERATION}" == "log" ]
+	then
+		systemd_log;
+		STATUS="${?}";
+		return "${STATUS}";
+	fi
+
+
+	if [ "${OPERATION}" == "status" ]
+	then
+		systemd_status;
+		STATUS="${?}";
+		return "${STATUS}";
+	fi
+
+
+	if [ "${OPERATION}" == "start" ]
+	then
+		systemd_start;
+		STATUS="${?}";
+		return "${STATUS}";
+	fi
+
+
+	if [ "${OPERATION}" == "stop" ]
+	then
+		systemd_stop;
+		STATUS="${?}";
+		return "${STATUS}";
+	fi
+
+
+	if [ "${OPERATION}" == "restart" ]
+	then
+		systemd_restart;
 		STATUS="${?}";
 		return "${STATUS}";
 	fi
