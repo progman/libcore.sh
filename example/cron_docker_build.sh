452b90d1068941aafb68cc1e068dda68a2f74b47
progman <gnuplanet@gmail.com>
2025-05-28 13:56:00 +0300

    add example

--- example/cron_docker_build.sh
+++ example/cron_docker_build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-function deploy()
+# WARNING! Use special/other git dir for build - if you use one dir then push will be not fetched/detected
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function build()
 {
 	export PS1="dirty_hack";
 	source /root/.bashrc;
 
 	export DOCKER_DIR="${1}";
 	export DOCKER_NOTIFY_MSG="${2}";
-	/usr/local/bin/docker_tool.sh deploy &> /dev/null < /dev/null;
-#	/usr/local/bin/docker_tool.sh deploy 2>&1 >> /tmp/docker_tool.log < /dev/null;
+	/usr/local/bin/git_tool_docker_builder.sh &> /dev/null < /dev/null;
+#	/usr/local/bin/git_tool_docker_builder.sh 2>&1 >> /tmp/git_tool_docker_builder.log < /dev/null;
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 export DOCKER_NOTIFY="/usr/local/bin/notify_telegram.sh";
@@ -16,7 +18,7 @@ export DOCKER_NOTIFY="/usr/local/bin/notify_telegram.sh";
 
 export TELEGRAM_BOT_TOKEN="HERE_TELEGRAM_BOT_TOKEN";
 export TELEGRAM_CHAT_ID="HERE_TELEGRAM_CHAT_ID";
-deploy 'HERE_DIR_OF_DOCKER_COMPOSE_CONFIG' "HERE_MESSAGE_FOR_TELEGRAM";
+build 'HERE_DIR_OF_GIT_REPO' "HERE_MESSAGE_FOR_TELEGRAM";
 
 
 sleep 10;
