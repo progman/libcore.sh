fd5e5108f741d6081ecd85e84105f3c207bf8dbf
progman <gnuplanet@gmail.com>
2025-09-10 22:14:47 +0300

    add to_whatsapp_video.sh

---
+++ to_whatsapp_video.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+if [ "${1}" == "" ] || [ "${2}" == "" ];
+then
+	echo "example: ${0} INPUT_VIDEO_FILE OUTPUT_VIDEO_FILE";
+	exit 1;
+fi
+
+
+ffmpeg -i "${1}" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -c:a aac "${2}.mp4";
+if [ "${?}" != "0" ];
+then
+	echo "ERROR";
+	exit 1;
+fi
+
+
+exit 0;
