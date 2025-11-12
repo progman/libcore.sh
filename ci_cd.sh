49ed659bb7cda406cae60c0bfda8db051db6930a
progman <gnuplanet@gmail.com>
2025-11-12 21:28:23 +0300

    add project_list

--- ci_cd.sh
+++ ci_cd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# 0.0.5
+# 0.0.6
 # Alexey Potehin <gnuplanet@gmail.com>, https://overtask.org/doc/cv
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # check depends
@@ -227,157 +227,171 @@ function ci_cd()
 	local STATUS;
 
 
-	echo;
+# project
+	PROJECT_LIST_INDEX=0;
+	PROJECT_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq '.project_list | length');
+	while true;
+	do
+		TEMPLATE_PROJECT=".project_list[${PROJECT_LIST_INDEX}]";
 
 
 # get TELEGRAM_BOT_TOKEN
-	local LOCAL_TELEGRAM_BOT_TOKEN;
-	LOCAL_TELEGRAM_BOT_TOKEN="$(cat ${CI_CD_CONFIG} | jq -r '.telegram.bot_token')";
-	if [ "${TELEGRAM_BOT_TOKEN}" == "" ];
-	then
-		export TELEGRAM_BOT_TOKEN="${LOCAL_TELEGRAM_BOT_TOKEN}";
-	fi
-	echo "TELEGRAM_BOT_TOKEN:${TELEGRAM_BOT_TOKEN}";
+		local LOCAL_TELEGRAM_BOT_TOKEN;
+		LOCAL_TELEGRAM_BOT_TOKEN=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_PROJECT}.telegram.bot_token");
+#		if [ "${TELEGRAM_BOT_TOKEN}" == "" ];
+#		then
+			export TELEGRAM_BOT_TOKEN="${LOCAL_TELEGRAM_BOT_TOKEN}";
+#		fi
+		echo "TELEGRAM_BOT_TOKEN:${TELEGRAM_BOT_TOKEN}";
 
 
 # get TELEGRAM_CHAT_ID
-	local LOCAL_TELEGRAM_CHAT_ID;
-	LOCAL_TELEGRAM_CHAT_ID="$(cat ${CI_CD_CONFIG} | jq -r '.telegram.chat_id')";
-	if [ "${TELEGRAM_CHAT_ID}" == "" ];
-	then
-		export TELEGRAM_CHAT_ID="${LOCAL_TELEGRAM_CHAT_ID}";
-	fi
-	echo "TELEGRAM_CHAT_ID:${TELEGRAM_CHAT_ID}";
+		local LOCAL_TELEGRAM_CHAT_ID;
+		LOCAL_TELEGRAM_CHAT_ID=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_PROJECT}.telegram.chat_id");
+#		if [ "${TELEGRAM_CHAT_ID}" == "" ];
+#		then
+			export TELEGRAM_CHAT_ID="${LOCAL_TELEGRAM_CHAT_ID}";
+#		fi
+		echo "TELEGRAM_CHAT_ID:${TELEGRAM_CHAT_ID}";
 
 
 # get DOCKER_REGISTRY_HOST
-	local LOCAL_DOCKER_REGISTRY_HOST;
-	LOCAL_DOCKER_REGISTRY_HOST="$(cat ${CI_CD_CONFIG} | jq -r '.docker_registry.host')";
-	if [ "${DOCKER_REGISTRY_HOST}" == "" ];
-	then
-		export DOCKER_REGISTRY_HOST="${LOCAL_DOCKER_REGISTRY_HOST}";
-	fi
-	echo "DOCKER_REGISTRY_HOST:${DOCKER_REGISTRY_HOST}";
+		local LOCAL_DOCKER_REGISTRY_HOST;
+		LOCAL_DOCKER_REGISTRY_HOST=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_PROJECT}.docker_registry.host");
+#		if [ "${DOCKER_REGISTRY_HOST}" == "" ];
+#		then
+			export DOCKER_REGISTRY_HOST="${LOCAL_DOCKER_REGISTRY_HOST}";
+#		fi
+		echo "DOCKER_REGISTRY_HOST:${DOCKER_REGISTRY_HOST}";
 
 
 # get DOCKER_REGISTRY_LOGIN
-	local LOCAL_DOCKER_REGISTRY_LOGIN;
-	LOCAL_DOCKER_REGISTRY_LOGIN="$(cat ${CI_CD_CONFIG} | jq -r '.docker_registry.login')";
-	if [ "${DOCKER_REGISTRY_LOGIN}" == "" ];
-	then
-		export DOCKER_REGISTRY_LOGIN="${LOCAL_DOCKER_REGISTRY_LOGIN}";
-	fi
-	echo "DOCKER_REGISTRY_LOGIN:${DOCKER_REGISTRY_LOGIN}";
+		local LOCAL_DOCKER_REGISTRY_LOGIN;
+		LOCAL_DOCKER_REGISTRY_LOGIN=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_PROJECT}.docker_registry.login");
+#		if [ "${DOCKER_REGISTRY_LOGIN}" == "" ];
+#		then
+			export DOCKER_REGISTRY_LOGIN="${LOCAL_DOCKER_REGISTRY_LOGIN}";
+#		fi
+		echo "DOCKER_REGISTRY_LOGIN:${DOCKER_REGISTRY_LOGIN}";
 
 
 # get DOCKER_REGISTRY_PASSWORD
-	local LOCAL_DOCKER_REGISTRY_PASSWORD;
-	LOCAL_DOCKER_REGISTRY_PASSWORD="$(cat ${CI_CD_CONFIG} | jq -r '.docker_registry.password')";
-	if [ "${DOCKER_REGISTRY_PASSWORD}" == "" ];
-	then
-		export DOCKER_REGISTRY_PASSWORD="${LOCAL_DOCKER_REGISTRY_PASSWORD}";
-	fi
-	echo "DOCKER_REGISTRY_PASSWORD:${DOCKER_REGISTRY_PASSWORD}";
+		local LOCAL_DOCKER_REGISTRY_PASSWORD;
+		LOCAL_DOCKER_REGISTRY_PASSWORD=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_PROJECT}.docker_registry.password");
+#		if [ "${DOCKER_REGISTRY_PASSWORD}" == "" ];
+#		then
+			export DOCKER_REGISTRY_PASSWORD="${LOCAL_DOCKER_REGISTRY_PASSWORD}";
+#		fi
+		echo "DOCKER_REGISTRY_PASSWORD:${DOCKER_REGISTRY_PASSWORD}";
 
 
 # fetch
-	FETCH_LIST_INDEX=0;
-	FETCH_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq '.fetch_list | length');
-	while true;
-	do
-#		echo "=====================================================================================================================";
-		TEMPLATE_FETCH=".fetch_list[${FETCH_LIST_INDEX}]";
-		FETCH_NAME=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_FETCH}.name");
-		FETCH_DIR=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_FETCH}.dir");
+		FETCH_LIST_INDEX=0;
+		FETCH_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq "${TEMPLATE_PROJECT}.fetch_list | length");
+		while true;
+		do
+#			echo "=====================================================================================================================";
+			TEMPLATE_FETCH="${TEMPLATE_PROJECT}.fetch_list[${FETCH_LIST_INDEX}]";
+			FETCH_NAME=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_FETCH}.name");
+			FETCH_DIR=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_FETCH}.dir");
 
-#		echo "FETCH_NAME:${FETCH_NAME}";
-#		echo "FETCH_DIR:${FETCH_DIR}";
-		echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]: try fetch \"${FETCH_NAME}\" in \"${FETCH_DIR}\"...";
+#			echo "FETCH_NAME:${FETCH_NAME}";
+#			echo "FETCH_DIR:${FETCH_DIR}";
+			echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]: try fetch \"${FETCH_NAME}\" in \"${FETCH_DIR}\"...";
+
 
 # git fetch
-		git_fetch "${FETCH_DIR}";
-		STATUS="${?}";
-		if [ "${STATUS}" != "0" ];
-		then
-			echo "skip repo";
-		else
-			echo "to be build";
+			git_fetch "${FETCH_DIR}";
+			STATUS="${?}";
+			if [ "${STATUS}" != "0" ];
+			then
+				echo "skip repo";
+			else
+				echo "to be build";
 
 
 # build docker images and push to registry
-			BUILD_STATUS="0"
-			BUILD_LIST_INDEX=0;
-			BUILD_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq "${TEMPLATE_FETCH}.build_list | length");
-			while true;
-			do
-#				echo ".....................................................................................................................";
-				TEMPLATE_BUILD="${TEMPLATE_FETCH}.build_list[${BUILD_LIST_INDEX}]";
-				BUILD_NAME=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_BUILD}.name");
-				BUILD_DIR=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_BUILD}.dir");
+				BUILD_STATUS="0"
+				BUILD_LIST_INDEX=0;
+				BUILD_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq "${TEMPLATE_FETCH}.build_list | length");
+				while true;
+				do
+#					echo ".....................................................................................................................";
+					TEMPLATE_BUILD="${TEMPLATE_FETCH}.build_list[${BUILD_LIST_INDEX}]";
+					BUILD_NAME=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_BUILD}.name");
+					BUILD_DIR=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_BUILD}.dir");
 
-#				echo "BUILD_NAME:${BUILD_NAME}";
-#				echo "BUILD_DIR:${BUILD_DIR}";
-				echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]: try build \"${BUILD_NAME}\" in \"${BUILD_DIR}\"...";
-
-
-				export DOCKER_DIR="${BUILD_DIR}";
-				export DOCKER_NOTIFY_MSG="${BUILD_NAME}";
-				echo "docker_tool.sh f;";
-				docker_tool.sh f; # save hash only if build is ok
-				STATUS="${?}";
-				if [ "${STATUS}" != "0" ];
-				then
-					BUILD_STATUS="${STATUS}";
-				fi
+#					echo "BUILD_NAME:${BUILD_NAME}";
+#					echo "BUILD_DIR:${BUILD_DIR}";
+					echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]: try build \"${BUILD_NAME}\" in \"${BUILD_DIR}\"...";
 
 
-				(( BUILD_LIST_INDEX++ ));
-				if [ ${BUILD_LIST_INDEX} -eq ${BUILD_LIST_COUNT} ];
-				then
-					break;
-				fi
-			done
+					export DOCKER_DIR="${BUILD_DIR}";
+					export DOCKER_NOTIFY_MSG="${BUILD_NAME}";
+					echo "docker_tool.sh f;";
+					docker_tool.sh f; # save hash only if build is ok
+					STATUS="${?}";
+					if [ "${STATUS}" != "0" ];
+					then
+						BUILD_STATUS="${STATUS}";
+					fi
+
+
+					(( BUILD_LIST_INDEX++ ));
+					if [ ${BUILD_LIST_INDEX} -eq ${BUILD_LIST_COUNT} ];
+					then
+						break;
+					fi
+				done
 
 
 # was all builds ok?
-			if [ "${BUILD_STATUS}" == "0" ];
-			then
-				git_save_hash "${FETCH_DIR}"; # ignore exit code
+				if [ "${BUILD_STATUS}" == "0" ];
+				then
+					git_save_hash "${FETCH_DIR}"; # ignore exit code
+				fi
 			fi
-		fi
 
 
-		(( FETCH_LIST_INDEX++ ));
-		if [ ${FETCH_LIST_INDEX} -eq ${FETCH_LIST_COUNT} ];
-		then
-			break;
-		fi
-	done
+			(( FETCH_LIST_INDEX++ ));
+			if [ ${FETCH_LIST_INDEX} -eq ${FETCH_LIST_COUNT} ];
+			then
+				break;
+			fi
+		done
 
 
 # deploy
-	DEPLOY_LIST_INDEX=0;
-	DEPLOY_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq '.deploy_list | length');
-	while true;
-	do
-#		echo "=====================================================================================================================";
-		TEMPLATE_DEPLOY=".deploy_list[${DEPLOY_LIST_INDEX}]";
-		DEPLOY_NAME=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_DEPLOY}.name");
-		DEPLOY_DIR=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_DEPLOY}.dir");
+		DEPLOY_LIST_INDEX=0;
+		DEPLOY_LIST_COUNT=$(cat ${CI_CD_CONFIG} | jq "${TEMPLATE_PROJECT}.deploy_list | length");
+		while true;
+		do
+#			echo "=====================================================================================================================";
+			TEMPLATE_DEPLOY="${TEMPLATE_PROJECT}.deploy_list[${DEPLOY_LIST_INDEX}]";
+			DEPLOY_NAME=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_DEPLOY}.name");
+			DEPLOY_DIR=$(cat ${CI_CD_CONFIG} | jq -r "${TEMPLATE_DEPLOY}.dir");
 
-#		echo "DEPLOY_NAME:${DEPLOY_NAME}";
-#		echo "DEPLOY_DIR:${DEPLOY_DIR}";
-		echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]: try deploy \"${DEPLOY_NAME}\" in \"${DEPLOY_DIR}\"...";
-
-
-		export DOCKER_DIR="${DEPLOY_DIR}";
-		export DOCKER_NOTIFY_MSG="${DEPLOY_NAME}";
-		echo "docker_tool.sh deploy;";
-		docker_tool.sh deploy; # skip return status code
+#			echo "DEPLOY_NAME:${DEPLOY_NAME}";
+#			echo "DEPLOY_DIR:${DEPLOY_DIR}";
+			echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]: try deploy \"${DEPLOY_NAME}\" in \"${DEPLOY_DIR}\"...";
 
 
-		(( DEPLOY_LIST_INDEX++ ));
-		if [ ${DEPLOY_LIST_INDEX} -eq ${DEPLOY_LIST_COUNT} ];
+			export DOCKER_DIR="${DEPLOY_DIR}";
+			export DOCKER_NOTIFY_MSG="${DEPLOY_NAME}";
+			echo "docker_tool.sh deploy;";
+			docker_tool.sh deploy; # skip return status code
+
+
+			(( DEPLOY_LIST_INDEX++ ));
+			if [ ${DEPLOY_LIST_INDEX} -eq ${DEPLOY_LIST_COUNT} ];
+			then
+				break;
+			fi
+		done
+
+
+		(( PROJECT_LIST_INDEX++ ));
+		if [ ${PROJECT_LIST_INDEX} -eq ${PROJECT_LIST_COUNT} ];
 		then
 			break;
 		fi
@@ -392,31 +406,35 @@ function help()
 {
 	echo "\$ cat /somedir/ci_cd.json";
 	echo "{";
-	echo "  \"telegram\": {";
-	echo "    \"bot_token\": \"TELEGRAM_BOT_TOKEN\",";
-	echo "    \"chat_id\": \"TELEGRAM_CHAT_ID\"";
-	echo "  },";
-	echo "  \"docker_registry\": {";
-	echo "    \"host\": \"127.0.0.1:5000\",";
-	echo "    \"login\": \"\",";
-	echo "    \"password\": \"\"";
-	echo "  },";
-	echo "  \"fetch_list\": [";
+	echo "  \"project_list\": [";
 	echo "    {";
-	echo "      \"name\": \"NAME_FOR_TELEGRAM_NOTIFY\",";
-	echo "      \"dir\": \"SPECIAL_DIR_WITH_CLONE_GIT_REPO_FOR_PULL_ONLY__YOU_CAN_NOT_COMMIT_AND_PUSH_AND_OTHER_HERE__!!!\",";
-	echo "      \"build_list\": [";
+	echo "      \"telegram\": {";
+	echo "        \"bot_token\": \"TELEGRAM_BOT_TOKEN\",";
+	echo "        \"chat_id\": \"TELEGRAM_CHAT_ID\"";
+	echo "      },";
+	echo "      \"docker_registry\": {";
+	echo "        \"host\": \"127.0.0.1:5000\",";
+	echo "        \"login\": \"\",";
+	echo "        \"password\": \"\"";
+	echo "      },";
+	echo "      \"fetch_list\": [";
 	echo "        {";
 	echo "          \"name\": \"NAME_FOR_TELEGRAM_NOTIFY\",";
-	echo "          \"dir\": \"DOCKER_BUILD_DIR\"";
+	echo "          \"dir\": \"SPECIAL_DIR_WITH_CLONE_GIT_REPO_FOR_PULL_ONLY__YOU_CAN_NOT_COMMIT_AND_PUSH_AND_OTHER_HERE__!!!\",";
+	echo "          \"build_list\": [";
+	echo "            {";
+	echo "              \"name\": \"NAME_FOR_TELEGRAM_NOTIFY\",";
+	echo "              \"dir\": \"DOCKER_BUILD_DIR\"";
+	echo "            }";
+	echo "          ]";
+	echo "        }";
+	echo "      ],";
+	echo "      \"deploy_list\": [";
+	echo "        {";
+	echo "          \"name\": \"NAME_FOR_TELEGRAM_NOTIFY\",";
+	echo "          \"dir\": \"DOCKER_COMPOSE_DIR\"";
 	echo "        }";
 	echo "      ]";
-	echo "    }";
-	echo "  ],";
-	echo "  \"deploy_list\": [";
-	echo "    {";
-	echo "      \"name\": \"NAME_FOR_TELEGRAM_NOTIFY\",";
-	echo "      \"dir\": \"DOCKER_COMPOSE_DIR\"";
 	echo "    }";
 	echo "  ]";
 	echo "}";
@@ -464,6 +482,7 @@ function main()
 
 
 	ci_cd;
+	echo;
 
 
 	return 0;
