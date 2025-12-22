2b7279ed7847f61ec8dc7e59dca12f6cd569768c
progman <gnuplanet@gmail.com>
2025-12-22 03:14:44 +0300

    update

--- to_whatsapp_video.sh
+++ to_whatsapp_video.sh
@@ -8,7 +8,8 @@ then
 fi
 
 
-ffmpeg -i "${1}" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -c:a aac "${2}.mp4";
+#ffmpeg -i "${1}" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -c:a aac "${2}.mp4";
+ffmpeg -i "${1}" -c:v libx264 -c:a aac -b:a 128k -movflags +faststart "${2}.mp4";
 if [ "${?}" != "0" ];
 then
 	echo "ERROR";
